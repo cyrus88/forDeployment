98ac17e19bdaeadcc627bd7f165de055049475c4
root <sushantsingh.tanwar@gmail.com>
2025-03-29 09:55:52 +0530

    first_commit

---
+++ src/main/java/com/sushant/SushantApplication.java
@@ -0,0 +1,13 @@
+package com.sushant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SushantApplication.class, args);
+	}
+
+}
